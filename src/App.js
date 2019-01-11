15692ab699825f7c115f28cdf90bec11ff5fd5e9
Kevin Sidhu <sidhu.kevin@gmail.com>
2019-01-10 19:26:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  submit = () => {
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Welcome text="Welcome to using Props" toggle={this.state.toggle} />
         </header>
+        <p className="App-intro">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <input type="text" />
+        <button onClick={this.submit}>Show Value</button>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
   }
 }
 
