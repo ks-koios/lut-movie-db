7223ce3a3fda4e96a32b038d00acfd49afb0bd16
Kevin Sidhu <sidhu.kevin@gmail.com>
2019-01-11 19:33:33 -0500

    inputs - controlled vs uncontrolled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    input: "Hello"
+  }
+
+  updateInput = (e) => {
+    this.setState({
+      input: e.target.value.trim()
+    })
+  }
+
   submit = () => {
     console.log(this.text.value)
   }
@@ -17,6 +27,8 @@ class App extends Component {
           <p className="App-intro">
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <h3>{this.state.input}</h3>
+          <input type="text" onChange={this.updateInput} value={this.state.input} />
           <input type="text" ref={(input) => this.text = input} />
           <button onClick={this.submit}>Show Value</button>
         </header>
